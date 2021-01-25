38516158a3d0504b66ab7c65ee01e2760c27892f
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2021-01-25 14:47:13 +0300

    Add files via upload

---
+++ ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+        }
+
+        public void List()
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Id: " + customer.Id + " ,Name: " + customer.Name + " , LastName: " + customer.LastName + " , Age: " + customer.Age);
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            }
+        }
+
+        public void Delete(Customer customer)
+        {
+            customers.Remove(customer);
+
+            Console.WriteLine("******"+"Customer Id Number " + customer.Id + " has been deleted.");
+        }
+      
+       
+    }
+}
