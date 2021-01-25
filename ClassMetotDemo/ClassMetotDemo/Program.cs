51ba2beba53b85a5ad123d5d0a5780622c57d3a5
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2021-01-25 14:51:08 +0300

    First Commit

---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                Id = 1,
+                Name = "Sümeyye",
+                LastName = "Kula",
+                Age = 23
+            };
+            Customer customer2 = new Customer()
+            {
+                Id = 2,
+                Name = "Ahmet",
+                LastName = "Özcan",
+                Age = 29
+            };
+            Customer customer3 = new Customer()
+            {
+                Id = 3,
+                Name = "Zeynep",
+                LastName = "Öztürk",
+                Age = 33
+            };
+            CustomerManager customer = new CustomerManager();
+            customer.Add(customer1);
+            customer.Add(customer2);
+            customer.Add(customer3);
+
+            customer.List();
+            customer.Delete(customer3);
+            customer.List();
+
+            Console.ReadLine();
+        }
+    }
+}
