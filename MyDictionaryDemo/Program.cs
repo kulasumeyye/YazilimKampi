a68af97b67b6f593dcf0e733569bb25462865982
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2021-01-27 00:12:59 +0300

    dictionary commit

---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDictionary<string, string> dictionary1 = new MyDictionary<string, string>();
+            dictionary1.Add("Mavi", "Blue");
+            dictionary1.Add("Yeşil", "Green");
+            dictionary1.Add("Kırmızı", "Red");
+            dictionary1.Add("Siyah", "Black");
+            dictionary1.Add("Beyaz", "White");
+
+        }
+    }
+}
